b5354680352841795140cf761aa0fda467831722
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-02 15:50:49 +0200

    Simplify polls admin

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
