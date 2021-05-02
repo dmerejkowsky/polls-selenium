6a4e9fd1cbd3dc996d1f10e50dcad0b112a6f8e6
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-02 15:49:27 +0200

    Step 1: make one test working

--- tests/tests_e2e.py
+++ tests/tests_e2e.py
@@ -1,7 +1,6 @@
 import selenium.webdriver
 import pytest
 
-
 ADMIN_USER = "test"
 ADMIN_PASSWORD = "test"
 
@@ -13,11 +12,12 @@ def driver():
     res.quit()
 
 
-def test_create_question():
-    driver = selenium.webdriver.Chrome()
+def test_create_question(driver):
+    question = "Is it working ?"
+    choice_1 = "yes"
+    choice_2 = "no"
 
     driver.get("http://127.0.0.1:8000/polls/reset/")
-
     driver.get("http://127.0.0.1:8000/admin")
     element = driver.find_element_by_id("id_username")
     element.send_keys(ADMIN_USER)
@@ -26,4 +26,25 @@ def test_create_question():
     element = driver.find_element_by_xpath('//input[@type="submit"]')
     element.click()
 
-    breakpoint()
+    driver.get("http://127.0.0.1:8000/admin/polls/question/add/")
+    element = driver.find_element_by_id("id_question_text")
+    element.send_keys(question)
+    element = driver.find_element_by_id("id_choice_set-0-choice_text")
+    element.send_keys(choice_1)
+    element = driver.find_element_by_id("id_choice_set-1-choice_text")
+    element.send_keys(choice_2)
+
+    element = driver.find_element_by_xpath('//input[@value="Save"]')
+    element.click()
+
+    driver.get("http://127.0.0.1:8000/polls")
+    element = driver.find_element_by_tag_name("a")
+    element.click()
+    element = driver.find_element_by_id("choice1")
+    element.click()
+    element = driver.find_element_by_xpath('//input[@value="Vote"]')
+    element.click()
+
+    page_source = driver.page_source
+    assert question in page_source
+    assert "yes -- 1 vote" in page_source
