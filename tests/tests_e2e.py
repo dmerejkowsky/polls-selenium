29934fc1910034a28a9a52cf64df41aaf93be124
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-03 17:45:54 +0200

    Rewrite using the Page design pattern

--- tests/tests_e2e.py
+++ tests/tests_e2e.py
@@ -5,6 +5,54 @@ ADMIN_USER = "test"
 ADMIN_PASSWORD = "test"
 
 
+class ResetPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def reset_db(self):
+        self.driver.get("http://127.0.0.1:8000/polls/reset/")
+
+
+class AdminPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.get("http://127.0.0.1:8000/admin")
+
+    def login(self, user, password):
+        element = self.driver.find_element_by_id("id_username")
+        element.send_keys(user)
+        element = self.driver.find_element_by_id("id_password")
+        element.send_keys(password)
+        element = self.driver.find_element_by_xpath('//input[@type="submit"]')
+        element.click()
+
+    def add_question(self, question, choices):
+        self.driver.get("http://127.0.0.1:8000/admin/polls/question/add/")
+        element = self.driver.find_element_by_id("id_question_text")
+        element.send_keys(question)
+
+        for i, choice in enumerate(choices):
+            element = self.driver.find_element_by_id(f"id_choice_set-{i}-choice_text")
+            element.send_keys(choice)
+
+        element = self.driver.find_element_by_xpath('//input[@value="Save"]')
+        element.click()
+
+
+class VotePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.get("http://127.0.0.1:8000/polls")
+
+    def vote_first_choice_on_first_question(self):
+        element = self.driver.find_element_by_tag_name("a")
+        element.click()
+        element = self.driver.find_element_by_id("choice1")
+        element.click()
+        element = self.driver.find_element_by_xpath('//input[@value="Vote"]')
+        element.click()
+
+
 @pytest.fixture
 def driver():
     res = selenium.webdriver.Chrome()
@@ -13,38 +61,16 @@ def driver():
 
 
 def test_create_question(driver):
-    question = "Is it working ?"
-    choice_1 = "yes"
-    choice_2 = "no"
+    reset_page = ResetPage(driver)
+    reset_page.reset_db()
 
-    driver.get("http://127.0.0.1:8000/polls/reset/")
-    driver.get("http://127.0.0.1:8000/admin")
-    element = driver.find_element_by_id("id_username")
-    element.send_keys(ADMIN_USER)
-    element = driver.find_element_by_id("id_password")
-    element.send_keys(ADMIN_PASSWORD)
-    element = driver.find_element_by_xpath('//input[@type="submit"]')
-    element.click()
+    admin_page = AdminPage(driver)
+    admin_page.login(ADMIN_USER, ADMIN_PASSWORD)
+    admin_page.add_question("Is it working?", ["yes", "no"])
 
-    driver.get("http://127.0.0.1:8000/admin/polls/question/add/")
-    element = driver.find_element_by_id("id_question_text")
-    element.send_keys(question)
-    element = driver.find_element_by_id("id_choice_set-0-choice_text")
-    element.send_keys(choice_1)
-    element = driver.find_element_by_id("id_choice_set-1-choice_text")
-    element.send_keys(choice_2)
-
-    element = driver.find_element_by_xpath('//input[@value="Save"]')
-    element.click()
-
-    driver.get("http://127.0.0.1:8000/polls")
-    element = driver.find_element_by_tag_name("a")
-    element.click()
-    element = driver.find_element_by_id("choice1")
-    element.click()
-    element = driver.find_element_by_xpath('//input[@value="Vote"]')
-    element.click()
+    vote_page = VotePage(driver)
+    vote_page.vote_first_choice_on_first_question()
 
     page_source = driver.page_source
-    assert question in page_source
+    assert "Is it working?" in page_source
     assert "yes -- 1 vote" in page_source
